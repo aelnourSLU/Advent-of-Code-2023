3f9e569b85ea120cf64b5830159f58787b6e20ae
Ali Elnour <ali.elnour@slu.edu>
2023-12-05 23:31:16 -0600

    this solution is unspeakably bad. FIX THIS later

--- src/d06/p1.rs
+++ src/d06/p1.rs
@@ -1,1 +1,45 @@
-pub fn start() {}
+use crate::util::file_reader;
+
+pub fn start() {
+    let input = file_reader("d06");
+    let input = input
+        .into_iter()
+        .map(|s| {
+            let split = s.split_terminator(": ");
+            let second = split.last();
+            let second = second
+                .into_iter()
+                .map(|s| {
+                    s.split_ascii_whitespace()
+                        .map(|s| s.trim().parse::<usize>().unwrap())
+                        .collect::<Vec<_>>()
+                })
+                .flatten()
+                .collect::<Vec<_>>();
+            second
+        })
+        .collect::<Vec<_>>();
+    println!("input: {:?}", input);
+    let mut out = Vec::new();
+    for i in 0..=input.len() + 1 {
+        let time = input[0][i];
+        let distance = input[1][i];
+        println!("Operating on time {}, distance {}", time, distance);
+        let mut hold = Vec::new();
+        for hold_time in 0..=time {
+            let ntime = time - hold_time;
+            let travel = hold_time * ntime;
+            hold.push(travel);
+        }
+        let hold = hold
+            .into_iter()
+            .filter(|&x| x > distance)
+            .collect::<Vec<_>>();
+        println!("hold: {}", hold.len());
+        out.push(hold.len())
+    }
+    println!("{:?}", input);
+    println!("{:?}", out.iter().product::<usize>());
+}
+
+// not 25600
--- src/d06/p2.rs
+++ src/d06/p2.rs
@@ -1,1 +1,41 @@
-pub fn start() {}
+use crate::util::file_reader;
+
+pub fn start() {
+    let input = file_reader("d06");
+    let input = input
+        .into_iter()
+        .map(|s| {
+            let split = s.split_terminator(": ");
+            let second = split.last();
+            let second = second
+                .unwrap()
+                .replace(" ", "")
+                .parse::<usize>()
+                .unwrap();
+            second
+        })
+        .collect::<Vec<_>>();
+    println!("input: {:?}", input);
+    let mut out = Vec::new();
+    for _ in 0..=input.len() {
+        let time = input[0];
+        let distance = input[1];
+        println!("Operating on time {}, distance {}", time, distance);
+        let mut hold = Vec::new();
+        for hold_time in 0..=time {
+            let ntime = time - hold_time;
+            let travel = hold_time * ntime;
+            hold.push(travel);
+        }
+        let hold = hold
+            .into_iter()
+            .filter(|&x| x > distance)
+            .collect::<Vec<_>>();
+        println!("hold: {}", hold.len());
+        out.push(hold.len())
+    }
+    println!("{:?}", input);
+    println!("{:?}", out.iter().product::<usize>());
+}
+
+// not 25600
