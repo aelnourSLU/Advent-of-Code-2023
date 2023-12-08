ddbd71aa85f179996575e0e8eb4d98b5622e0007
Ali Elnour <ali.elnour@slu.edu>
2023-12-08 00:13:17 -0600

    day 8 solved (ugly)

--- src/d08/p2.rs
+++ src/d08/p2.rs
@@ -32,57 +32,70 @@ pub fn start() {
         .chars()
         .collect::<VecDeque<_>>();
 
-    /* let mut curr_node_str = "AAA";
-    let mut count = 0;
-    while curr_node_str != "ZZZ" {
-        let next_dir = directions.pop_front().unwrap();
-        directions.push_back(next_dir.to_owned());
-        let curr_node = node_hm.get(curr_node_str).unwrap();
-        match next_dir {
-            'L' => curr_node_str = curr_node.left.as_str(),
-            'R' => curr_node_str = curr_node.right.as_str(),
-            _ => panic!()
-        }
-        println!("Going {} to {}", next_dir, curr_node_str);
-        count += 1;
-    } */
-
-    let mut count = 0;
     let mut curr_node_str_vec = nodes
         .iter()
         .filter(|n| n.ends_a())
         .map(|n| n.name.clone())
         .collect::<Vec<_>>();
-    
-    let all_end_z = |v: &Vec<String>, start: usize| { 
-        start == v.iter()
-            .filter(|&s| s.chars().last().unwrap() == 'Z')
-            .collect::<Vec<_>>()
-            .len()
-    };
-    let num_start_nodes = curr_node_str_vec.len();
-
-    while !all_end_z(&curr_node_str_vec, num_start_nodes) {
-        let next_dir = directions.pop_front().unwrap();
-        directions.push_back(next_dir.to_owned());
-        let mut new_nodes = Vec::new();
-        for node in curr_node_str_vec {
-            let curr_node = node_hm.get(&node).unwrap();
-            let to_node;
-            match next_dir {
-                'L' => to_node = curr_node.left.clone(),
-                'R' => to_node = curr_node.right.clone(),
-                _ => panic!()
+ 
+    let end_z = |s: &str| { s.chars().last().unwrap() == 'Z' };
+    let results = curr_node_str_vec
+        .iter()
+        .map(|node| {
+            let mut count = 0;
+            let mut curr_node_str = node.to_owned();
+            while !end_z(curr_node_str.as_str()) {
+                let next_dir = directions.pop_front().unwrap();
+                directions.push_back(next_dir.to_owned());
+                let curr_node = node_hm.get(&curr_node_str).unwrap();
+                match next_dir {
+                    'L' => curr_node_str = curr_node.left.clone(),
+                    'R' => curr_node_str = curr_node.right.clone(),
+                    _ => panic!()
+                }
+                // println!("Going {} to {}", next_dir, curr_node_str);
+                count += 1;
             }
-            //println!("Going {} from {} to {}", next_dir, curr_node.name, to_node);
-            new_nodes.push(to_node);
-        }
-        //println!("==");
-        curr_node_str_vec = new_nodes;
-        count += 1;
-    }
+            count
+        })
+        .collect::<Vec<_>>();
+    let highest = results.iter().max().unwrap();
+    let lcm = results
+        .iter()
+        .map(|x| *x as usize)
+        .reduce(|acc, e| lcm(acc, e))
+        .unwrap();
+    // println!("Count: {}", count);
+    println!("Out: {:?}", highest);
+    println!("lcm: {}", lcm);
+}
 
-    println!("Count: {}", count);
+fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    let buffer = a;
+    let a_og = a;
+    let b_og = b;
+    let a = if b > a {
+        b
+    } else {
+        a
+    };
+    
+    let b = if b_og > a_og {
+        buffer
+    } else {
+        b
+    };
+    
+    let d = a % b;
+    if d == 0 {
+        b
+    } else {
+        gcd(b, d)
+    }
 }
 
 #[derive(Debug)]
@@ -105,3 +118,5 @@ impl Node {
         self.name.chars().last().unwrap() == 'Z'
     }
 }
+
+// not 1917116283508457903
